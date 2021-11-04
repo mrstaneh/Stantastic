7eb5e1bfcc47a9fc4bfb4c823cf81689099dd17b
Stan Jaworski <staneh@live.nl>
2021-11-04 23:27:58 +0100

    Update about.svelte

--- stantastic-site/src/routes/about.svelte
+++ stantastic-site/src/routes/about.svelte
@@ -15,7 +15,7 @@
     </div>
     <div class="about-content">
         <div class="about-text">
-            <p>Hallo! Mijn naam is Stan. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tincidunt at metus sit amet pulvinar. Proin feugiat consequat orci, ut bibendum velit venenatis et. Sed semper iaculis sem eget volutpat.</p>
+            <p>Hello! My name is Stan. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tincidunt at metus sit amet pulvinar. Proin feugiat consequat orci, ut bibendum velit venenatis et. Sed semper iaculis sem eget volutpat.</p>
             <p>Nam tortor lorem, accumsan vitae diam at, rutrum interdum ex. Praesent auctor sollicitudin malesuada. Sed ac sem augue. Donec porta pretium massa, at ultricies dolor rhoncus a.</p>
             <p>Ut finibus eget erat ut elementum. Quisque bibendum dignissim nisi, quis gravida nisl egestas ac. Etiam dictum, tortor vitae commodo rhoncus, leo mauris ultricies urna.</p>
             <p>Cras a mi in urna interdum bibendum:</p>
@@ -80,7 +80,7 @@
     }
 
     .title-divider{
-        padding-top: 40px;
+        padding-top: 20px;
         margin-right: 36px;
         height: 100%;
         width: 100%;
@@ -93,6 +93,8 @@
 
     .about-title h1{
         width: 180px;
+        margin-block-start: 0em;
+        margin-block-end: 0em;
     }
 
     .about-content{
