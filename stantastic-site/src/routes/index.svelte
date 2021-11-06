e1ed6c9b0f1613a3654a5efefd63f5c661da2fb5
Stan Jaworski <staneh@live.nl>
2021-11-06 22:21:53 +0100

    Update index.svelte

--- stantastic-site/src/routes/index.svelte
+++ stantastic-site/src/routes/index.svelte
@@ -1,10 +1,23 @@
+<script>
+	import { currentLanguage } from '$lib/stores.js';
+	import { fade } from 'svelte/transition';
+</script>
+
 <svelte:head>
 	<title>Home</title>
 </svelte:head>
 
 <div class="home-banner">
 	<h1>Stan</h1><h1 class="h1-p2">tastic</h1>
-	<p class="subtitle">Op maat gemaakte software</p>
+	{#if $currentLanguage == 'en'}
+		<p class="subtitle" in:fade={{duration: 250}} >
+				Tailor-made websites
+		</p>
+	{:else if $currentLanguage == 'nl'}
+		<p class="subtitle" in:fade={{duration: 250}} >
+			Op maat gemaakte websites
+		</p>
+	{/if}
 </div>
 
 <style>
