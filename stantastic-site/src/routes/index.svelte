ee3cbe9dc14523fd1d44274412ddec4eb5007d3e
Stan Jaworski <staneh@live.nl>
2021-11-06 20:42:05 +0100

    Update index.svelte

--- stantastic-site/src/routes/index.svelte
+++ stantastic-site/src/routes/index.svelte
@@ -4,17 +4,30 @@
 
 <div class="home-banner">
 	<h1>Stan</h1><h1 class="h1-p2">tastic</h1>
+	<p class="subtitle">Op maat gemaakte software</p>
 </div>
 
 <style>
+	.home-banner{
+		margin-top: 5%;
+	}
+
+	p, h1{
+		font-family: 'Titillium Web', sans-serif;
+	}
+
     h1{
 		display: inline;
-        font-family: 'Titillium Web', sans-serif;
         color: #4A4A55;
-		font-size: 64px;
+		font-size: 80px;
     }
 
 	.h1-p2{
 		color: #EF8320;
+	}
+
+	.subtitle{
+		font-size: 18px;
+		margin-top: 0px;
 	}
 </style>
