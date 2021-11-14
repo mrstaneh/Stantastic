4dd0ad6877ce0f1d8e450e70800f6f16de55da89
Stan Jaworski <staneh@live.nl>
2021-11-14 22:02:59 +0100

    updates

--- stantastic-site/src/routes/index.svelte
+++ stantastic-site/src/routes/index.svelte
@@ -161,7 +161,7 @@
 
 		window.requestAnimFrame=function(){return window.requestAnimationFrame||window.webkitRequestAnimationFrame||window.mozRequestAnimationFrame||window.oRequestAnimationFrame||window.msRequestAnimationFrame||function(a){window.setTimeout(a,1E3/60)}}();
 
-		loop();
+		//loop();
 	}
 
 	function clickDownButton(){
--- stantastic-site/svelte.config.js
+++ stantastic-site/svelte.config.js
@@ -1,9 +1,15 @@
 import node from '@sveltejs/adapter-node';
+//import adapter from '@sveltejs/adapter-static';
 
 const config = {
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
+		/*adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		})*/
+
 		adapter: node()
 	}
 };
