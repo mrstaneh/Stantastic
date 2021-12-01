80638d858769b1135caed6109a8bd342a8cab29d
Stan Jaworski <staneh@live.nl>
2021-12-01 09:35:18 +0100

    Update svelte.config.js

--- stantastic-site/svelte.config.js
+++ stantastic-site/svelte.config.js
@@ -1,21 +1,21 @@
-import node from '@sveltejs/adapter-node';
-//import adapter from '@sveltejs/adapter-static';
-import adapter from 'svelte-adapter-deno';
+//import node from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
+//import adapter from 'svelte-adapter-deno';
 
 const config = {
 	kit: {
-		/*adapter: adapter({
+		adapter: adapter({
 			// default options are shown
 			pages: 'build',
 			assets: 'build',
 			fallback: null
-		})*/
+		})
 
-		adapter: adapter({
+		/*adapter: adapter({
 			// default options are shown
 			out: 'build',
 			deps: './node_modules/svelte-adapter-deno/deps.ts' // (relative to adapter-deno package)
-		})
+		})*/
 
 		//adapter: node()
 	}
