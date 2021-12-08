ad6feeda1c0004702e79cd068c2f1d7de855f16c
Stan Jaworski <staneh@live.nl>
2021-12-08 22:56:34 +0100

    possible crash fix

--- stantastic-site/src/routes/index.svelte
+++ stantastic-site/src/routes/index.svelte
@@ -11,10 +11,12 @@
     });
 
 	function displayWindowSize(){
-		var c = document.getElementById('canvas');
+		try{
+			var c = document.getElementById('canvas');
 
-		c.style.width = document.documentElement.clientWidth.toString() + "px";
-		c.style.height = document.documentElement.clientHeight.toString() + "px";
+			c.style.width = document.documentElement.clientWidth.toString() + "px";
+			c.style.height = document.documentElement.clientHeight.toString() + "px";
+		}catch{}
 	}
 
 	const map = (value, x1, y1, x2, y2) => (value - x1) * (y2 - x2) / (y1 - x1) + x2;
