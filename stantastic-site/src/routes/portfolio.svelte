35eca1e9ba553cd962583c88b974426bcb306290
Stan Jaworski <staneh@live.nl>
2021-12-08 22:43:09 +0100

    added portfolio section

--- stantastic-site/src/routes/portfolio.svelte
+++ stantastic-site/src/routes/portfolio.svelte
@@ -13,20 +13,88 @@
     </div>
     <div class="section-content">
         <div class="section-text">
-            {#if $currentLanguage == 'en'}
-                <p>Hello! My name is Stan. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tincidunt at metus sit amet pulvinar. Proin feugiat consequat orci, ut bibendum velit venenatis et. Sed semper iaculis sem eget volutpat.</p>
-                <p>Nam tortor lorem, accumsan vitae diam at, rutrum interdum ex. Praesent auctor sollicitudin malesuada. Sed ac sem augue. Donec porta pretium massa, at ultricies dolor rhoncus a.</p>
-                <p>Ut finibus eget erat ut elementum. Quisque bibendum dignissim nisi, quis gravida nisl egestas ac. Etiam dictum, tortor vitae commodo rhoncus, leo mauris ultricies urna.</p>
-            {:else if $currentLanguage == 'nl'}
-                <p>Hallo! Mijn naam is Stan. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tincidunt at metus sit amet pulvinar. Proin feugiat consequat orci, ut bibendum velit venenatis et. Sed semper iaculis sem eget volutpat.</p>
-                <p>Nam tortor lorem, accumsan vitae diam at, rutrum interdum ex. Praesent auctor sollicitudin malesuada. Sed ac sem augue. Donec porta pretium massa, at ultricies dolor rhoncus a.</p>
-                <p>Ut finibus eget erat ut elementum. Quisque bibendum dignissim nisi, quis gravida nisl egestas ac. Etiam dictum, tortor vitae commodo rhoncus, leo mauris ultricies urna.</p>
-            {/if}
+            <div class="project-list">
+                <div class="project">
+                        <div class="image-container">
+                            <a href="https://boulderarchive.com/" target="_blank">
+                                <img src="boulderarchive.jpg" alt="Boulder Archive">
+                            </a>
+                        </div>
+                    <div class="info-container">
+                        <div class="project-subtitle">
+                            <p>
+                                {#if $currentLanguage == 'en'}
+                                    Own Project
+                                {:else if $currentLanguage == 'nl'}
+                                    Eigen Project
+                                {/if}
+                            </p>
+                        </div>
+                        <div class="project-title">
+                            <h1><a href="https://boulderarchive.com/" target="_blank">Boulder Archive</a></h1>
+                        </div>
+                        <div class="project-info">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tincidunt at metus sit amet pulvinar.</p>
+                        </div>
+                        <div class="project-tools">
+                            <ul class="tools-list">
+                                <li>Svelte</li>
+                                <li>Sapper</li>
+                                <li>.NET Core</li>
+                                <li>AWS</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </section>
 
 <style>
+    .project{
+        display: flex;
+        margin-top: 1em;
+        padding-left: 1em;
+        padding-right: 1.8em;
+    }
+
+    .image-container{
+        display: flex;
+        width: 75%;
+        height: 100%;
+        border-radius: 8px;
+        mix-blend-mode: multiply;
+        background-color: grey;
+    }
+
+    .image-container:hover{
+        cursor: pointer;
+        background-color: transparent
+    }
+
+    .project-subtitle p{
+        margin: 0.5em 0em -0.2em 0em;
+    }
+
+    .project-title h1{
+        margin-top: 0;
+        margin-bottom: 0.5em;
+        font-size: 30px;
+    }
+
+    .image-container:hover{
+        background-color: transparent;
+    }
+
+    .project img{
+        opacity: 0.90;
+        vertical-align: top;
+        width: 100%;
+        filter: contrast(1) brightness(50%);
+        border-radius: 8px;
+    }
+
     .title-divider{
         padding-top: 20px;
         margin-right: 36px;
@@ -58,8 +126,43 @@
         font-size: 18px;
     }
 
-    p, h1{
-        font-family: 'Titillium Web', sans-serif;
-        color: black;
+    .info-container{
+        display: flex;
+        flex-direction: column;
+        width: 50%;
+        text-align: right;
+        justify-content: center;
+    }
+
+    .info-container p{
+        text-align: right;
+    }
+
+    .project-info{
+        position: relative;
+        left: -70%;
+        width: 160%;
+        float: right;
+        border: 1px solid #7b7b8f;
+        background-color: #d8dde0;
+        border-radius: 4px;
+        padding: 1em;
+    }
+
+    .project-info p{
+        margin: 0;
+        text-align: left;
+    }
+
+    .tools-list{
+        list-style: none;
+    }
+
+    li{
+        display: inline;
+        border: 1px solid #7b7b8f;
+        border-radius: 4px;
+        padding: 0px 6px 0px 6px;
+        background-color: #d8dde0;
     }
 </style>
