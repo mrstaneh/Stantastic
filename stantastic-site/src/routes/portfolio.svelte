2924e4a1ba88c8798ea8c1f57db7fdd6a1989a72
Stan Jaworski <staneh@live.nl>
2021-12-13 00:11:14 +0100

    Update portfolio.svelte

--- stantastic-site/src/routes/portfolio.svelte
+++ stantastic-site/src/routes/portfolio.svelte
@@ -13,42 +13,10 @@
     </div>
     <div class="section-content">
         <div class="section-text">
-            <div class="card-container">
-                <p>hoi</p>
-            </div>
         </div>
     </div>
 </section>
 
 <style>
-    .title-divider{
-        padding-top: 20px;
-        margin-right: 36px;
-        height: 100%;
-        width: 100%;
-        border-bottom: 1px solid black;
-    }
 
-    .section-header{
-        display: flex;
-    }
-
-    .section-header h1{
-        width: 180px;
-        margin-block-start: 0em;
-        margin-block-end: 0em;
-    }
-
-    .section-content{
-        display: flex;
-    }
-
-    .section-text{
-        float: left;
-    }
-
-    .section-text p{
-        text-align: left;
-        font-size: 18px;
-    }
 </style>
